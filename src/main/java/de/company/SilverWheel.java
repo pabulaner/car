94f02001c9257a0a5ead4a14f503fd2070651949
happymeal2024 <167612772+happymeal2024@users.noreply.github.com>
2024-07-23 14:04:34 +0200

    Car 240723
    Car 240723

---
+++ src/main/java/de/company/SilverWheel.java
@@ -0,0 +1,12 @@
+package de.vimo;
+
+public class SilverWheel extends Wheel {
+    int red = 0;
+    int blue = 90;
+    int green = 10;
+
+    public String getSilverWheel() {
+        return "wheel is silver with red " + red + " blue " + blue + " and green " + green;
+    }
+
+}
---
+++ src/main/java/de/company/Wheel.java
@@ -0,0 +1,6 @@
+package de.vimo;
+
+public class Wheel {
+
+
+}
